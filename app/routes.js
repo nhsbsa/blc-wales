c68e9cdef422a8bcedca643849589caaff69c5ef
Roshine Bagha <Ka.roshine.bagha@nhsbsa.nhs.uk>
2026-01-30 10:28:53 +0000

    Moving files to V1 Folder

--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,6 @@ router.post('/location-answer', function(request, response) {
     }
 })
 
-
 router.post('/main-relation-to-baby', function(request, response) {
     var relation = request.session.data['relationshipToBaby']
     if (relation == "none"){
@@ -49,6 +48,7 @@ router.post('/security-code-method', function(request, response) {
     }
 })
 
+
 router.post('/confirm-post-address', function(request, response) {
     var method = request.session.data['confirmAddress']
     if (method == "yes"){
@@ -57,6 +57,7 @@ router.post('/confirm-post-address', function(request, response) {
         response.redirect("/your-details/check-your-details-kickout")
     }
 })
+
 
 router.post('/add-another-parent', function(request, response) {
     var method = request.session.data['addAnotherParent']
@@ -135,5 +136,132 @@ router.post('/2-certs/baby-2-include-sex', function(request, response) {
 
 
 
+////////////////////////////
+///------- MAIN V1 -------//
+///////////////////////////
 
 
+router.post('/mainV1-location-answer', function(request, response) {
+    var location = request.session.data['country-location']
+    if (location == "other"){
+        response.redirect("/main-V1/eligibility/location-kickout-uk")
+    } else {
+        response.redirect("/main-V1/eligibility/relation-to-baby")
+    }
+})
+
+router.post('/mainV1-main-relation-to-baby', function(request, response) {
+    var relation = request.session.data['relationshipToBaby']
+    if (relation == "none"){
+        response.redirect("/main-V1/eligibility/relation-kickout")
+    } else {
+        response.redirect("/main-V1/your-details/nhs-number")
+    }
+})
+
+router.post('/mainV1-know-nhs-number', function(request, response) {
+    var knowNHSNum = request.session.data['knowNhsNumber']
+  
+    if (knowNHSNum == "yes" || knowNHSNum == "ydw"  ){
+      
+        response.redirect("/main-V1/your-details/enter-nhs-number")
+    } else {
+        response.redirect("/main-V1/your-details/what-is-your-name")
+        
+    }
+})
+
+router.post('/mainV1-security-code-method', function(request, response) {
+    var method = request.session.data['seccode']
+    if (method == "email"){
+        response.redirect("/main-V1/verification/enter-security-code-email")
+    } else {
+        response.redirect("/main-V1/verification/enter-security-code-text")
+    }
+})
+
+
+router.post('/mainV1-confirm-post-address', function(request, response) {
+    var method = request.session.data['confirmAddress']
+    if (method == "yes"){
+        response.redirect("/main-V1/other-parent-details/add-parent")
+    } else {
+        response.redirect("/main-V1/your-details/check-your-details-kickout")
+    }
+})
+
+
+router.post('/mainV1-add-another-parent', function(request, response) {
+    var method = request.session.data['addAnotherParent']
+    if (method == "yes"){
+        response.redirect("/main-V1/other-parent-details/what-happens-next")
+    } else {
+        response.redirect("/main-V1/pregnancy-details/what-happens-next")
+    }
+})
+
+router.post('/mainV1-number-of-babies-lost', function(request, response) {
+    var method = request.session.data['loss-multiple']
+    if (method == "1-cert"){
+        response.redirect("/main-V1/1-cert/date-of-loss")
+    } else if (method == "twin"){ 
+        response.redirect("/main-V1/pregnancy-details/twin-loss")
+    } else{
+        response.redirect("/main-V1/pregnancy-details/triplet-loss")
+    }
+})
+
+router.post('/mainV1-baby-sex-choice-1-cert', function(request, response) {
+    var method = request.session.data['gender']
+    if (method == "yes"){
+        response.redirect("/main-V1/1-cert/sex-of-baby")
+    }  else{
+        response.redirect("/main-V1/1-cert/name-of-baby")
+    }
+})
+
+
+router.post('/mainV1-confirmation-1-cert-another-cert', function(request, response) {
+    var method = request.session.data['certreq']
+    if (method == "yes"){
+        response.redirect("/main-V1/add-other-parent")
+    }  else{
+        response.redirect("/main-V1/confirmation-page-new-1-cert")
+    }
+})
+
+router.post('/mainV1-twin-losses', function(request, response) {
+    var method = request.session.data['bothbabies']
+    if (method == "yes"){
+        response.redirect("/main-V1/2-certs/baby-1/date-of-loss")
+    }  else{
+        response.redirect("/main-V1/1-cert/date-of-loss")
+    }
+})
+
+router.post('/mainV1-triplet-losses', function(request, response) {
+    var method = request.session.data['triplet-loss']
+    if (method == "single"){
+         response.redirect("/main-V1/1-cert/date-of-loss")
+    }  else{
+        response.redirect("/main-V1/2-certs/baby-1/date-of-loss")
+    }
+})
+
+router.post('/mainV1-2-certs/baby-1-include-sex', function(request, response) {
+    var method = request.session.data['include-sex']
+    if (method == "yes"){
+        response.redirect("/main-V1/2-certs/baby-1/sex-of-baby")
+    }  else{
+        response.redirect("/main-V1/2-certs/baby-1/name-of-baby")
+    }
+})
+
+router.post('/mainV1-2-certs/baby-2-include-sex', function(request, response) {
+    var method = request.session.data['include-sex']
+    if (method == "yes"){
+        response.redirect("/main-V1/2-certs/baby-2/sex-of-baby")
+    }  else{
+        response.redirect("/main-V1/2-certs/baby-2/name-of-baby")
+    }
+})
